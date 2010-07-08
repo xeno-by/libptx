ad3777cfec03b6bdf9da087eb05f02ac38e609a3
Eugene Burmako <xeno.by@gmail.com>
2010-07-08 15:54:56 +0300

    Upgraded Libcuda auto-build script

--- ThirdPartyStuff/libcuda/build-libcuda.bat
+++ ThirdPartyStuff/libcuda/build-libcuda.bat
@@ -6,21 +6,20 @@ if not "%2"=="" (
     exit /B 1
 )
 
-if "%1"=="" (
-    echo [Fatal error] Too little arguments! Please, provide a single argument to this batch file: desired build configuration ^(it can be either "Debug" or "Release"^).
-    pause
-    exit /B 1
-) else ( 
+SET BUILDCONFIG=%1
+if "%1"=="" SET /P BUILDCONFIG=Specify build configuration ^(it can be either "Debug" or "Release"^): 
 
-if "%1"=="Debug" (
+if "%BUILDCONFIG%"=="Debug" (
+    SET BUILDCONFIG=Debug
     echo Building Libcuda using Debug configuration...
-) else ( if "%1"=="Release" (
+) else ( if "%BUILDCONFIG%"=="Release" (
+    SET BUILDCONFIG=Release
     echo Building Libcuda using Release configuration...
 ) else (
-    echo [Fatal error] Unsupported build configuration "%1". Only "Debug" or "Release" are supported.
-    @pause
+    echo [Fatal error] Unsupported build configuration "%BUILDCONFIG%". Only "Debug" or "Release" are supported.
+    pause
     exit /B 1
-)))
+))
 
 if exist Sources rmdir Sources /s /q
 mkdir Sources
@@ -37,18 +36,17 @@ hg clone https://xenogears.googlecode.com/hg/ XenoGears -r e9e9791dc334
 echo.
 echo ^>^>^>^>^> Building Libcuda...
 cd Libcuda\Libcuda
-"%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild" /t:Rebuild /p:Configuration=%1
+"%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild" /t:Rebuild /p:Configuration=%BUILDCONFIG%
 
 echo.
 echo ^>^>^>^>^> Ilmerging Libcuda...
 cd ..\..\..\
 if not exist Binaries mkdir Binaries
 cd Binaries
-if exist %1 rmdir %1 /s /q
-mkdir %1
-cd %1
-ilmerge /t:library /out:Libcuda.dll ..\..\Sources\Libcuda\Libcuda\bin\%1\Libcuda.dll ..\..\Sources\Libcuda\Libcuda\bin\%1\XenoGears.dll /internalize /log
+if exist %BUILDCONFIG% rmdir %BUILDCONFIG% /s /q
+mkdir %BUILDCONFIG%
+cd %BUILDCONFIG%
+ilmerge /t:library /out:Libcuda.dll ..\..\Sources\Libcuda\Libcuda\bin\%BUILDCONFIG%\Libcuda.dll ..\..\Sources\Libcuda\Libcuda\bin\%BUILDCONFIG%\XenoGears.dll /internalize /log
 
 cd ..\..
 rmdir Sources /s /q
-@pause
