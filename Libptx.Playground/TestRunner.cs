c4e1d51c426666b7d96d66308f11e2b96cdbad44
Eugene Burmako <xeno.by@gmail.com>
2010-07-08 14:38:59 +0300

    Initialized project structure

---
+++ Libptx.Playground/TestRunner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Libptx.Playground
+{
+    internal class TestRunner
+    {
+        public static void Main(String[] args)
+        {
+            // see more details at http://www.nunit.org/index.php?p=consoleCommandLine&r=2.5.5
+            var nunitArgs = new List<String>();
+            nunitArgs.Add("/run:Libptx.Playground");
+            nunitArgs.Add("/include:Hot");
+            nunitArgs.Add("/domain:None");
+            nunitArgs.Add("/noshadow");
+            nunitArgs.Add("/nologo");
+            nunitArgs.Add("Libptx.Playground.exe");
+            NUnit.ConsoleRunner.Runner.Main(nunitArgs.ToArray());
+        }
+    }
+}
---
+++ ThirdPartyStuff/xenogears/readme.txt
@@ -0,0 +1,6 @@
+﻿This project requires XenoGears source code rather than binaries.
+Expected source location: ..\XenoGears (relative to the root of this checkout)
+You can get source code as follows: hg clone https://xenogears.googlecode.com/hg/
+
+I'm using such a simple mechanism of referencing a 3rd-party codebase since at the moment XenoGears is still very much in flux.
+Thus, to make it easier to spot and fix bugs I've decided to use this code sharing scheme despite of possible instability introduced by totally uncontrollable reference.
