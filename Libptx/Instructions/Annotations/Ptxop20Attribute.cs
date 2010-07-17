65e87c3c2223c8dd4b32ad5496277c9c28a6a625
Eugene Burmako <xeno.by@gmail.com>
2010-07-18 01:18:35 +0300

    Refactoring PTX object model

--- Libptx/Instructions/Annotations/Ptxop20Attribute.cs
+++ Libptx/Instructions/Annotations/Ptxop20Attribute.cs
@@ -1,50 +1,61 @@
 ﻿using System;
 using System.Diagnostics;
 using Libcuda.Versions;
-using Libptx.Common.Annotations.Atoms;
+using XenoGears.Assertions;
 
 namespace Libptx.Instructions.Annotations
 {
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
     [DebuggerNonUserCode]
-    internal class Ptxop20Attribute : Atom20Attribute
+    internal class Ptxop20Attribute : PtxopAttribute
     {
         public Ptxop20Attribute()
+            : this(null, SoftwareIsa.PTX_20, HardwareIsa.SM_20)
         {
         }
 
-        public Ptxop20Attribute(SoftwareIsa swisa)
-            : base(swisa)
+        public Ptxop20Attribute(SoftwareIsa softwareIsa)
+            : this(null, softwareIsa.AssertThat(isa => isa >= SoftwareIsa.PTX_20), HardwareIsa.SM_20)
         {
         }
 
-        public Ptxop20Attribute(HardwareIsa hwisa)
-            : base(hwisa)
+        public Ptxop20Attribute(HardwareIsa hardwareIsa)
+            : this(null, SoftwareIsa.PTX_20, hardwareIsa)
         {
         }
 
-        public Ptxop20Attribute(SoftwareIsa swisa, HardwareIsa hwisa)
-            : base(swisa, hwisa)
+        public Ptxop20Attribute(SoftwareIsa softwareIsa, HardwareIsa hardwareIsa)
+            : this(null, softwareIsa.AssertThat(isa => isa >= SoftwareIsa.PTX_20), hardwareIsa.AssertThat(isa => isa >= HardwareIsa.SM_20))
         {
         }
 
-        public Ptxop20Attribute(String spec)
-            : base(spec)
+        public Ptxop20Attribute(HardwareIsa hardwareIsa, SoftwareIsa softwareIsa)
+            : this(null, softwareIsa.AssertThat(isa => isa >= SoftwareIsa.PTX_20), hardwareIsa.AssertThat(isa => isa >= HardwareIsa.SM_20))
         {
         }
 
-        public Ptxop20Attribute(String spec, SoftwareIsa swisa)
-            : base(spec, swisa)
+        public Ptxop20Attribute(String signature)
+            : this(signature, SoftwareIsa.PTX_20, HardwareIsa.SM_20)
         {
         }
 
-        public Ptxop20Attribute(String spec, HardwareIsa hwisa)
-            : base(spec, hwisa)
+        public Ptxop20Attribute(String signature, SoftwareIsa softwareIsa)
+            : this(signature, softwareIsa.AssertThat(isa => isa >= SoftwareIsa.PTX_20), HardwareIsa.SM_10)
         {
         }
 
-        public Ptxop20Attribute(String spec, SoftwareIsa swisa, HardwareIsa hwisa)
-            : base(spec, swisa, hwisa)
+        public Ptxop20Attribute(String signature, HardwareIsa hardwareIsa)
+            : this(signature, SoftwareIsa.PTX_20, hardwareIsa.AssertThat(isa => isa >= HardwareIsa.SM_20))
+        {
+        }
+
+        public Ptxop20Attribute(String signature, SoftwareIsa softwareIsa, HardwareIsa hardwareIsa)
+            : base(signature, softwareIsa.AssertThat(isa => isa >= SoftwareIsa.PTX_20), hardwareIsa.AssertThat(isa => isa >= HardwareIsa.SM_20))
+        {
+        }
+
+        public Ptxop20Attribute(String signature, HardwareIsa hardwareIsa, SoftwareIsa softwareIsa)
+            : base(signature, softwareIsa.AssertThat(isa => isa >= SoftwareIsa.PTX_20), hardwareIsa.AssertThat(isa => isa >= HardwareIsa.SM_20))
         {
         }
     }
--- Libptx/Instructions/Annotations/PtxopAttribute.cs
+++ Libptx/Instructions/Annotations/PtxopAttribute.cs
@@ -10,42 +10,52 @@ namespace Libptx.Instructions.Annotations
     internal class PtxopAttribute : AtomAttribute
     {
         public PtxopAttribute()
-            : base()
+            : this(null, SoftwareIsa.PTX_10, HardwareIsa.SM_10)
         {
         }
 
-        public PtxopAttribute(SoftwareIsa swisa)
-            : base(swisa)
+        public PtxopAttribute(SoftwareIsa softwareIsa)
+            : this(null, softwareIsa, softwareIsa < SoftwareIsa.PTX_20 ? HardwareIsa.SM_10 : HardwareIsa.SM_20)
         {
         }
 
-        public PtxopAttribute(HardwareIsa hwisa)
-            : base(hwisa)
+        public PtxopAttribute(HardwareIsa hardwareIsa)
+            : this(null, hardwareIsa < HardwareIsa.SM_20 ? SoftwareIsa.PTX_10 : SoftwareIsa.PTX_20, hardwareIsa)
         {
         }
 
-        public PtxopAttribute(SoftwareIsa swisa, HardwareIsa hwisa)
-            : base(swisa, hwisa)
+        public PtxopAttribute(SoftwareIsa softwareIsa, HardwareIsa hardwareIsa)
+            : this(null, softwareIsa, hardwareIsa)
         {
         }
 
-        public PtxopAttribute(String spec)
-            : base(spec)
+        public PtxopAttribute(HardwareIsa hardwareIsa, SoftwareIsa softwareIsa)
+            : this(null, softwareIsa, hardwareIsa)
         {
         }
 
-        public PtxopAttribute(String spec, SoftwareIsa swisa)
-            : base(spec, swisa)
+        public PtxopAttribute(String signature)
+            : this(signature, SoftwareIsa.PTX_10, HardwareIsa.SM_10)
         {
         }
 
-        public PtxopAttribute(String spec, HardwareIsa hwisa)
-            : base(spec, hwisa)
+        public PtxopAttribute(String signature, SoftwareIsa softwareIsa)
+            : this(signature, softwareIsa, softwareIsa < SoftwareIsa.PTX_20 ? HardwareIsa.SM_10 : HardwareIsa.SM_20)
         {
         }
 
-        public PtxopAttribute(String spec, SoftwareIsa swisa, HardwareIsa hwisa)
-            : base(spec, swisa, hwisa)
+        public PtxopAttribute(String signature, HardwareIsa hardwareIsa)
+            : this(signature, hardwareIsa < HardwareIsa.SM_20 ? SoftwareIsa.PTX_10 : SoftwareIsa.PTX_20, hardwareIsa)
+        {
+        }
+
+        public PtxopAttribute(String signature, SoftwareIsa softwareIsa, HardwareIsa hardwareIsa)
+            : base(signature, softwareIsa, hardwareIsa)
+        {
+        }
+
+        public PtxopAttribute(String signature, HardwareIsa hardwareIsa, SoftwareIsa softwareIsa)
+            : base(signature, softwareIsa, hardwareIsa)
         {
         }
     }
