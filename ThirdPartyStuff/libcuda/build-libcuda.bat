7c0c7059e42498da0457c4924930ea9b938ca79c
Eugene Burmako <xeno.by@gmail.com>
2010-07-18 20:30:05 +0300

    Abstracting away the location of XenoGears' codebase

--- ThirdPartyStuff/libcuda/build-libcuda.bat
+++ ThirdPartyStuff/libcuda/build-libcuda.bat
@@ -27,16 +27,41 @@ cd Sources
 
 echo.
 echo ^>^>^>^>^> Downloading Libcuda sources from https://libcuda.googlecode.com/hg/
-hg clone https://libcuda.googlecode.com/hg/ Libcuda -r a899aee51751
+hg clone https://libcuda.googlecode.com/hg/ Libcuda -r ee82e7e6e326
+if not exist Libcuda (
+    echo [Fatal error] Failed to get Libcuda sources.
+    pause
+    cd ..\
+    exit /B 1
+)
 
 echo.
 echo ^>^>^>^>^> Downloading XenoGears sources from https://xenogears.googlecode.com/hg/
-hg clone https://xenogears.googlecode.com/hg/ XenoGears -r b1bd7ae9d315
+hg clone https://xenogears.googlecode.com/hg/ XenoGears -r 6eaf1dc095d2
+if not exist XenoGears (
+    echo [Fatal error] Failed to get XenoGears sources.
+    pause
+    cd ..\
+    exit /B 1
+)
+
+echo.
+echo ^>^>^>^>^> Preparing Libcuda...
+cd Libcuda
+..\..\change Libcuda.sln "%%XENOGEARS%%" "..\XenoGears"
+..\..\change Libcuda\Libcuda.csproj "$(XENOGEARS)" "..\..\XenoGears"
+..\..\change Libcuda.Playground\Libcuda.Playground.csproj "$(XENOGEARS)" "..\..\XenoGears"
 
 echo.
 echo ^>^>^>^>^> Building Libcuda...
-cd Libcuda\Libcuda
+cd Libcuda
 "%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild" /t:Rebuild /p:Configuration=%BUILDCONFIG%
+if not exist bin\%BUILDCONFIG%\Libcuda.dll (
+    echo [Fatal error] Failed to build Libcuda.
+    pause
+    cd ..\..\..\
+    exit /B 1
+)
 
 echo.
 echo ^>^>^>^>^> Ilmerging Libcuda...
@@ -47,6 +72,12 @@ if exist %BUILDCONFIG% rmdir %BUILDCONFIG% /s /q
 mkdir %BUILDCONFIG%
 cd %BUILDCONFIG%
 ilmerge /t:library /out:Libcuda.dll ..\..\Sources\Libcuda\Libcuda\bin\%BUILDCONFIG%\Libcuda.dll ..\..\Sources\Libcuda\Libcuda\bin\%BUILDCONFIG%\XenoGears.dll /internalize /log
+if not exist Libcuda.dll (
+    echo [Fatal error] Failed to ilmerge Libcuda.
+    pause
+    cd ..\..
+    exit /B 1
+)
 
 cd ..\..
 rmdir Sources /s /q
