c412e5acec82a85f69e52152a7877288bb10731f
Eugene Burmako <xeno.by@gmail.com>
2010-08-01 18:18:05 +0300

    Upgraded build scripts: XenoGears is no longer ilmerged into dependencies, since it DOES need to: a) be shared between all libraries within a process, b) be exposed to potential clients

--- ThirdPartyStuff/libcuda/build-libcuda.bat
+++ ThirdPartyStuff/libcuda/build-libcuda.bat
@@ -36,25 +36,8 @@ if not exist Libcuda (
 )
 
 echo.
-echo ^>^>^>^>^> Downloading XenoGears sources from https://xenogears.googlecode.com/hg/
-hg clone https://xenogears.googlecode.com/hg/ XenoGears -r 6eaf1dc095d2
-if not exist XenoGears (
-    echo [Fatal error] Failed to get XenoGears sources.
-    pause
-    cd ..\
-    exit /B 1
-)
-
-echo.
-echo ^>^>^>^>^> Preparing Libcuda...
-cd Libcuda
-..\..\change Libcuda.sln "%%XENOGEARS%%" "..\XenoGears"
-..\..\change Libcuda\Libcuda.csproj "$(XENOGEARS)" "..\..\XenoGears"
-..\..\change Libcuda.Playground\Libcuda.Playground.csproj "$(XENOGEARS)" "..\..\XenoGears"
-
-echo.
 echo ^>^>^>^>^> Building Libcuda...
-cd Libcuda
+cd Libcuda\Libcuda
 "%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild" /t:Rebuild /p:Configuration=%BUILDCONFIG%
 if not exist bin\%BUILDCONFIG%\Libcuda.dll (
     echo [Fatal error] Failed to build Libcuda.
@@ -63,21 +46,5 @@ if not exist bin\%BUILDCONFIG%\Libcuda.dll (
     exit /B 1
 )
 
-echo.
-echo ^>^>^>^>^> Ilmerging Libcuda...
-cd ..\..\..\
-if not exist Binaries mkdir Binaries
-cd Binaries
-if exist %BUILDCONFIG% rmdir %BUILDCONFIG% /s /q
-mkdir %BUILDCONFIG%
-cd %BUILDCONFIG%
-ilmerge /t:library /out:Libcuda.dll ..\..\Sources\Libcuda\Libcuda\bin\%BUILDCONFIG%\Libcuda.dll ..\..\Sources\Libcuda\Libcuda\bin\%BUILDCONFIG%\XenoGears.dll /internalize /log
-if not exist Libcuda.dll (
-    echo [Fatal error] Failed to ilmerge Libcuda.
-    pause
-    cd ..\..
-    exit /B 1
-)
-
-cd ..\..
+cd ..\..\..
 rmdir Sources /s /q
