9de353cd7c25b27f9c7b0622dad8ab779dbc4400
Eugene Burmako <xeno.by@gmail.com>
2010-08-03 11:08:36 +0300

    Synchronizing e-DSL projects with massive changes in Libptx

---
+++ Libptx.Edsl/Libptx.Edsl.TextGenerators/AdHoc/ConstGenerator.cs
@@ -0,0 +1,10 @@
+namespace Libptx.Edsl.TextGenerators.AdHoc
+{
+    internal static class ConstGenerator
+    {
+        public static void DoGenerate()
+        {
+            // todo. see Libptx.Edsl\Expressions\Immediate\const.cs for more info on what to do
+        }
+    }
+}
--- Libptx.Edsl/Libptx.Edsl.TextGenerators/Program.cs
+++ Libptx.Edsl/Libptx.Edsl.TextGenerators/Program.cs
@@ -8,26 +8,25 @@ namespace Libptx.Edsl.TextGenerators
     {
         public static void Main(String[] args)
         {
+            using (new Context(SoftwareIsa.PTX_21, HardwareIsa.SM_20))
+            {
+                PtxoptypeGenerator.DoGenerate();
+                SregtypeGenerator.DoGenerate();
 
+                TypeGenerator.DoGenerate();
+                ConstGenerator.DoGenerate();
+                VectorGenerator.DoGenerate();
+                SregGenerator.DoGenerate();
 
-//            using (new Context(SoftwareIsa.PTX_21, HardwareIsa.SM_20))
-//            {
-//                PtxoptypeGenerator.DoGenerate();
-//                SpecialtypeGenerator.DoGenerate();
-//
-//                TypeGenerator.DoGenerate();
-//                SpecialGenerator.DoGenerate();
-//                VectorGenerator.DoGenerate();
-//
-//                // todo:
-//                // 1) compile-time finite automaton for creating ptxops
-//                // 2) compile-time finite automaton for declaring vars
-//                // 3) we also need to type constants and provide casts for them as well
-//                // 4) there should be a way to emit both named and unnamed vars (reg.u32 and reg.u32("foo"))
-//                // 5) there should be a way to infer var names from the names of locals (debug-mode only, of course)
-//                // 6) the latter should be optional since it implies serious performance degradation
-//                // 7) don't forget to emit [DebuggerNonUserCode]
-//            }
+                // todo:
+                // 1) compile-time finite automaton for creating ptxops
+                // 2) compile-time finite automaton for declaring regs and vars
+                // 3) we also need to type constants and provide casts for them as well
+                // 4) there should be a way to emit both named and unnamed regs and vars (reg.u32 and reg.u32("foo"))
+                // 5) there should be a way to infer reg and var names from the names of locals (debug-mode only, of course)
+                // 6) the latter should be optional since it implies serious performance degradation
+                // 7) don't forget to emit [DebuggerNonUserCode]
+            }
         }
     }
 }
